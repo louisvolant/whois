067c8bcf51c3d119f0a81e8f4a1f8ffc1b2554a3
Louis Volant <volantlouis@gmail.com>
2025-12-28 11:33:58 +0100

    Updated footer links

--- frontend/src/app/links.ts
+++ frontend/src/app/links.ts
@@ -12,7 +12,8 @@ export const externalLinks: LinkItem[] = [
   { label: 'Personal Page', href: 'https://www.louisvolant.com' },
   { label: 'Password Keeper', href: 'https://www.securaised.net' },
   { label: 'Skipass Checker', href: 'https://skipass-earlybird-checker.louisvolant.com' },
-  { label: 'Rain Under The Cloud', href: 'https://rainunderthe.cloud' },
+  { label: 'Sun Over The Cloud', href: 'https://sunoverthe.cloud' },
   { label: 'Build My CV', href: 'https://buildmycv.net' },
   { label: 'My 20 years old blog', href: 'https://www.abricocotier.fr' },
+  { label: 'Currency Converter', href: 'https://currency-converter-pwa-js.vercel.app/' },
 ];
